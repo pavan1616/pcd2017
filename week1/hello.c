ac73236c863790be8289d1288b6001b3c760bca0
Your Name <you@example.com>
2017-02-17 07:56:56 +0530

    add2

---
+++ week1/hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int add(int a , int b)
+{
+return a+b;
+}
+int main()
+{
+printf("%dn",add(1,2));
+}
+
+
---
+++ week1/sum3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int sum(int a,int b,int c)
+{
+return a+b+c;
+}
+int main()
+{
+printf("%d/n",sum(1,2,3));
+}
+
